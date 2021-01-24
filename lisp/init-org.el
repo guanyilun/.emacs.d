28322a2059ff74400abccf6883639602df7beb97
Yilun Guan <zoom.aaron@gmail.com>
2021-01-24 11:34:45 -0500

    refactor tex related configs from init-org into init-tex

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,29 +6,19 @@
          ("C-c b" . org-switchb)
          ("C-c l" . org-store-link)
          ("C-c c" . org-capture))
-  :init (setq org-highlight-latex-and-related '(entities)
-              org-latex-pdf-process
-              '("pdflatex -shell-escape -interaction nonstopmode -f %f"
-                "bibtex %b"
-                "pdflatex -shell-escape -interaction nonstopmode -f %f"
-                "pdflatex -shell-escape -interaction nonstopmode -f %f")
-              ;; org babel
-              org-confirm-babel-evaluate nil
-              ;; attachment
-              org-attach-dir-relative   t
-              org-src-fontify-natively  t
-              org-src-tab-acts-natively t
-              ;; inline image size default
-              ;; for image specific setting use
-              ;; #+ATTR_ORG: :width 400
-              org-image-actual-width '(400)
-              org-attach-auto-tag nil)
+  :init (setq 
+         ;; org babel
+         org-confirm-babel-evaluate nil
+         ;; attachment
+         org-attach-dir-relative   t
+         org-src-fontify-natively  t
+         org-src-tab-acts-natively t
+         ;; inline image size default
+         ;; for image specific setting use
+         ;; #+ATTR_ORG: :width 400
+         org-image-actual-width '(400)
+         org-attach-auto-tag nil)
   :config
-  ;; latex support
-  (require 'ox-latex)
-  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5)
-        org-latex-prefer-user-labels t)
-
   ;; Prettify UI
   (use-package org-bullets
     :if (char-displayable-p ?⚫)
@@ -46,39 +36,12 @@
   (org-babel-do-load-languages 'org-babel-load-languages
                                load-language-list)
 
-  ;; toggle fragment when leaving the src block
-  (use-package org-fragtog
-    :hook (org-mode . org-fragtog-mode))
-
-  ;; org equation live preview
-  (use-package org-elp
-    :ensure nil
-    :load-path "~/.emacs.d/site-lisp/"
-    :init
-    (require 'org-elp))
-
   ;; org-ref to manage references
   ;; see https://github.com/jkitchin/org-ref/blob/master/org-ref.org
   ;; for more information
   (use-package org-ref
     :init (require 'org-ref))
   
-  ;; Enlarge tex fragments at the same time as text scale
-  (defun update-org-latex-fragment-scale ()
-    (let ((text-scale-factor (expt text-scale-mode-step text-scale-mode-amount)))
-      (plist-put org-format-latex-options :scale (* 1.5 text-scale-factor)))
-    )
-  (add-hook 'text-scale-mode-hook 'update-org-latex-fragment-scale)
-  ;; revtex template
-  (add-to-list 'org-latex-classes
-               '("revtex4-1"
-                 "\\documentclass{revtex4-1}"
-                 ("\\section{%s}" . "\\section*{%s}")
-                 ("\\subsection{%s}" . "\\subsection*{%s}")
-                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
   ;; org download
   (use-package org-download
     :bind (("C-S-y" . org-download-clipboard))
@@ -91,8 +54,7 @@
     (add-hook 'dired-mode-hook 'org-download-enable))
 
   ;; always use evince to open pdf
-  (setcdr (assoc "\\.pdf\\'" org-file-apps) "evince %s")
-  )
+  (setcdr (assoc "\\.pdf\\'" org-file-apps) "evince %s"))
 
 (provide 'init-org)
 
--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -1,9 +1,49 @@
 (use-package auctex
   :config
   ;; this depends on auctex
-  (use-package cdlatex
-    :hook (org-mode . turn-on-org-cdlatex))
   ;; set preview scale
   (set-default 'preview-scale-function 1.2))
+
+;; org latex related
+(use-package cdlatex
+  :hook (org-mode . turn-on-org-cdlatex))
+
+(require 'ox-latex)
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5)
+      org-latex-prefer-user-labels t)
+
+;; toggle fragment when leaving the src block
+(use-package org-fragtog
+  :hook (org-mode . org-fragtog-mode))
+
+;; org equation live preview
+(use-package org-elp
+  :ensure nil
+  :load-path "~/.emacs.d/site-lisp/"
+  :init
+  (require 'org-elp))
+
+(setq org-highlight-latex-and-related '(entities)
+      org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -f %f"
+        "bibtex %b"
+        "pdflatex -shell-escape -interaction nonstopmode -f %f"
+        "pdflatex -shell-escape -interaction nonstopmode -f %f"))
+
+;; Enlarge tex fragments at the same time as text scale
+(defun update-org-latex-fragment-scale ()
+  (let ((text-scale-factor (expt text-scale-mode-step text-scale-mode-amount)))
+    (plist-put org-format-latex-options :scale (* 1.5 text-scale-factor)))
+  )
+(add-hook 'text-scale-mode-hook 'update-org-latex-fragment-scale)
+;; revtex template
+(add-to-list 'org-latex-classes
+             '("revtex4-1"
+               "\\documentclass{revtex4-1}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 (provide 'init-tex)
