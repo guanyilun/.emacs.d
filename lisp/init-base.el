32bf271ec49822220aa7a6eb97653db123456e76
Yilun Guan <zoom.aaron@gmail.com>
2022-01-10 23:00:40 -0500

    init-base: add elfeed

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -67,4 +67,40 @@
        (propertize title 'face face))))
   (require 'pretty-hydra))
 
+(use-package elfeed
+  :pretty-hydra
+  ((:title (pretty-hydra-title "Elfeed" 'faicon "rss-square")
+           :color amaranth :quit-key "q")
+   ("Search"
+    (("g" elfeed-search-update--force "refresh")
+     ("G" elfeed-search-fetch "update")
+     ("y" elfeed-search-yank "copy URL")
+     ("+" elfeed-search-tag-all "tag all")
+     ("-" elfeed-search-untag-all "untag all"))
+    "Filter"
+    (("s" elfeed-search-live-filter "live filter")
+     ("S" elfeed-search-set-filter "set filter")
+     ("*" (elfeed-search-set-filter "@6-months-ago +star") "starred")
+     ("A" (elfeed-search-set-filter "@6-months-ago" "all"))
+     ("T" (elfeed-search-set-filter "@1-day-ago" "today")))
+    "Article"
+    (("b" elfeed-search-browse-url "browse")
+     ("n" next-line "next")
+     ("p" previous-line "previous")
+     ("u" elfeed-search-tag-all-unread "mark unread")
+     ("r" elfeed-search-untag-all-unread "mark read")
+     ("RET" elfeed-search-show-entry "show"))))
+  :bind (("C-x w" . elfeed)
+         :map elfeed-search-mode-map
+         ("?" . elfeed-hydra/body)
+         :map elfeed-show-mode-map
+         ("o" . ace-link)
+         ("q" . delete-window))
+  :config
+  (setq elfeed-db-directory (locate-user-emacs-file ".elfeed")
+        elfeed-show-entry-switch #'pop-to-buffer
+        elfeed-show-entry-delete #'delete-window)
+  ;; a trick to adjust elfeed fill column
+  (setq shr-width 80))
+
 (provide 'init-base)
