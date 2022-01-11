7d1e14297bbec4fca28d5d2b39643dfe46abc06f
Yilun Guan <zoom.aaron@gmail.com>
2022-01-10 23:00:23 -0500

    init-roam: use v2

--- lisp/init-roam.el
+++ lisp/init-roam.el
@@ -11,32 +11,28 @@
 ;; the variable org-roam-directory needs to be customized
 (use-package org-roam
   :ensure t
-  :bind (
-         (("C-c n l" . org-roam-buffer-toggle)
-          ("C-c n f" . org-roam-node-find)
-          ("C-c n g" . org-roam-graph)
-          ("C-c n r" . org-roam-random-note)
-          ("C-c n c" . org-roam-capture)
-          ("C-c n o" . my/org-roam-knowledge-search))
-         :map org-mode-map
-         (("C-c n i" . org-roam-node-insert)))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-ui-mode)
+         ("C-c n r" . org-roam-node-random)
+         ("C-c n R" . org-roam-refile)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n o" . my/org-roam-knowledge-search)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n I" . org-id-get-create)
+         ("C-c n a" . org-roam-alias-add)
+         ("C-c n t" . org-roam-tag-add))
   :config
   (org-roam-db-autosync-mode)
   (org-add-link-type "ebib" 'ebib)
 
-  ;; (use-package org-roam-bibtex
-  ;;   :ensure t)
-  ;; ;; (use-package org-roam-bibtex
-  ;; ;;   :ensure t
-  ;; ;;   :hook (org-roam-mode . org-roam-bibtex-mode))
   (use-package org-roam-bibtex
     :after org-roam)
   
   (setq org-roam-capture-templates
-        '(("d" "default" plain (function org-roam--capture-get-point)
+        '(("d" "default" plain ;; (function org-roam--capture-get-point)
            "%?"
-           :file-name "${slug}"
-           :head "#+setupfile: ../config.setup\n#+title: ${title}\n#+date: %<%Y-%m-%d>\n\nbibliography:../bib/citation.bib\n\n"
+           :target (file+head "${slug}.org" "#+setupfile: ../config.setup\n#+title: ${title}\n#+date: %<%Y-%m-%d>\n\nbibliography:../bib/citation.bib\n\n")
            :unnarrowed t)))
 
   (defun my/org-roam-knowledge-search ()
@@ -55,16 +51,44 @@
     (setq deft-extensions '("org" "bib" "txt")
           deft-directory org-roam-directory
           deft-recursive t
-          deft-use-filename-as-title t))
-
-  (use-package helm-rg)
-  )
+          deft-use-filename-as-title t)))
 
 (use-package org-roam-ui
+  :bind (:map org-mode-map
+              ("C-c n z" . org-roam-ui-node-zoom))
   :config
   (setq org-roam-ui-sync-theme t
         org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
+        org-roam-ui-update-on-save t))
+;;        org-roam-ui-open-on-start nil))
+
+(use-package helm-org-rifle
+  :ensure t
+  :bind (("C-c n r" . my/helm-org-rifle-roam-rifle))
+  :config
+  (defun my/helm-org-rifle-roam-rifle ()
+    "Use roam directory rifle"
+    (interactive)
+    (helm-org-rifle-directories org-roam-directory))
+
+  (defun my/helm-org-rifle--store-link (candidate)
+    "Store link into CANDIDATE."
+    (-let (((buffer . pos) candidate))
+      (with-current-buffer buffer
+        (goto-char pos)
+        (call-interactively 'org-store-link))))
+
+  (defun my/helm-org-rifle--insert-link (candidate)
+    "Insert link to CANDIDATE in current location."
+    (interactive)
+    (my/helm-org-rifle--store-link candidate)
+    (call-interactively 'org-insert-link))
+
+  ;; add new actions to the default rifle action list
+  (setq helm-org-rifle-actions
+        (append helm-org-rifle-actions
+                (helm-make-actions
+                 "Store link" 'my/helm-org-rifle--store-link
+                 "Insert link" 'my/helm-org-rifle--insert-link))))
 
 (provide 'init-roam)
